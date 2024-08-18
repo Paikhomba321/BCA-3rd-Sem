a0f485adc791430e23c819cda5342ff7f4a30e6f
Paikhomba Nongmeikpam <paikhombanongmeikapam53@gmail.com>
2024-08-18 19:11:46 +0530

    #rd Sem HTML

---
+++ Index/Newspaper.js
@@ -0,0 +1,12 @@
+function showLoginForm() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('signupForm').style.display = 'none';
+  }
+  
+  function showSignupForm() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signupForm').style.display = 'block';
+  }
+  
+  // You can add more JavaScript functions for form validation, news upload, etc.
+  
